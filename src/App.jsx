5636cb966e6f5e57f34a01b5e0f1f1d3086fca74
agentbari2 <agentbari2@gmail.com>
2022-12-01 20:41:09 +0300

    Multi search with || operators

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,23 @@
 import { useState } from 'react';
 import './app.css';
+import Table from './table';
 import {Users} from './users';
 
 function App(){
   const [query, setQuery] = useState('');
 
+  console.log(Users[4].email)
+
+  const  search = (data) => {
+    return data.filter((item) => item.first_name.toLowerCase().includes(query) || 
+    item.last_name.toLowerCase().includes(query) || 
+    item.email.toLowerCase().includes(query));
+  }
   return(
     <div className="app">
       <input type="text" placeholder="Search..." className="search" onChange={(e) => setQuery(e.target.value)}/>
 
-        <ul className="list">
-          {Users.filter((user) => user.first_name.
-          toLowerCase().includes(query))
-          .map((user) => (
-          <li key={user.id} className="listItem">
-            {user.first_name}
-          </li>
-          ))}
-        </ul>
+      <Table data={search(Users)}/>
     </div>
   );
 }
