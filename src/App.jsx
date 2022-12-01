e55aeeab60453f215f88f19d9da98584abcf662b
agentbari2 <agentbari2@gmail.com>
2022-12-01 20:59:27 +0300

    Changed || operators to the 'some' function

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,14 @@ import {Users} from './users';
 function App(){
   const [query, setQuery] = useState('');
 
-  console.log(Users[4].email)
+  const keys = ['first_name, last_name, email'];
 
-  const  search = (data) => {
-    return data.filter((item) => item.first_name.toLowerCase().includes(query) || 
-    item.last_name.toLowerCase().includes(query) || 
-    item.email.toLowerCase().includes(query));
-  }
+  const search = (data) => {
+    return data.filter((item) =>
+      keys.some((key) => item[key].toLowerCase().includes(query))
+    );
+};
+
   return(
     <div className="app">
       <input type="text" placeholder="Search..." className="search" onChange={(e) => setQuery(e.target.value)}/>
